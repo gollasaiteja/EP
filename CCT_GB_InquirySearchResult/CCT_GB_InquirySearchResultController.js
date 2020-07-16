e394ec73c4d473be759396fbcb9051591409264f
swathikonatham <32087148+swathikonatham@users.noreply.github.com>
2020-07-16 10:09:57 -0500

    v2

--- CCT_GB_InquirySearchResult/CCT_GB_InquirySearchResultController.js
+++ CCT_GB_InquirySearchResult/CCT_GB_InquirySearchResultController.js
@@ -1,17 +1,25 @@
 ({
 	doInit : function(component, event, helper) {
-		//alert('here');
+		alert('here');
+        var myPageRef = component.get("v.pageReference");
+        var inquiryRes = myPageRef && myPageRef.state ? myPageRef.state.c__InquirySearchResult : "World";
+        component.set("v.InquirySearchResult", inquiryRes);
         //alert(component.get("v.InquirySearchResult"));
-        var inquiryResult = component.get("v.InquirySearchResult");
-        //alert(JSON.stringify(inquiryResult));
+        var inquiryResult = JSON.parse(component.get("v.InquirySearchResult"));
+        alert(inquiryResult);
+        alert(JSON.stringify(inquiryResult));
         var generalRes = inquiryResult.general;
         //alert(JSON.stringify(generalRes));
-        alert(JSON.stringify(inquiryResult.internet));
+        //alert(JSON.stringify(inquiryResult.internet));
         component.set("v.generalResult",generalRes);
         component.set("v.internetResult",inquiryResult.internet);
         component.set("v.notesResult",inquiryResult.notes);
         component.set("v.emailRepliesResult",inquiryResult.emailedReplies);
         component.set("v.logHistoryResult",inquiryResult.logHistory);
         
-	}
-})
+	},
+    
+    handlePageChange : function(component, event, helper){
+        alert('in page change');
+    }
+});
